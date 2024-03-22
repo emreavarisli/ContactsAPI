7be9fb84d0a62f468a23b58a59e629de87e6931f
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2024-03-22 12:23:29 +0300

    get and post

---
+++ ContactsAPI/Controllers/ContactController.cs
@@ -0,0 +1,44 @@
+﻿using ContactsAPI.Data;
+using ContactsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : Controller
+    {
+        private readonly ContactsAPIDbContext dbContext;
+
+        public ContactController(ContactsAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetContacts()
+        {
+            return Ok(await dbContext.Contacts.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContact(AddContactRequest addContactRequest)
+        {
+            var contact = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Address = addContactRequest.Address,
+                Email = addContactRequest.Email,
+                FullName = addContactRequest.FullName,
+                Phone = addContactRequest.Phone,
+            };
+
+            await dbContext.Contacts.AddAsync(contact);
+            await dbContext.SaveChangesAsync(); 
+
+            return Ok(contact);
+        }
+
+    }
+}
